a609f2655e9010a7099d3b97ecb518ef60144e9d
Asadullah <asadullahashraf3030@gmailcom>
2025-08-27 20:19:14 +0500

    Add Second Again

---
+++ Script.js
@@ -0,0 +1,24 @@
+const largeImg = document.querySelector('.large-img')
+const smallImg = document.querySelectorAll('.small-img')
+
+for(const elem of smallImg){
+        elem.onclick = function (){
+        largeImg.src = elem.src
+    }
+}
+
+const loginForm = document.querySelector('#loginForm')
+const regForm = document.querySelector('#regForm')
+const indeCater = document.querySelector('#indecator')
+
+function register() {
+    regForm.style.transform = 'translateX(0px)'
+    loginForm.style.transform = 'translateX(0px)'
+    indeCater.style.transform = 'translateX(100px)'
+}
+
+function login() {
+    regForm.style.transform = 'translateX(300px)'
+    loginForm.style.transform = 'translateX(300px)'
+    indeCater.style.transform = 'translateX(0px)'
+}
